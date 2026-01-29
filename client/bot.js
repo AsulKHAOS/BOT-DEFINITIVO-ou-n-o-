3b2e5f97142858e52df80b5996c9eb49628c39ac
AsulKHAOS <AsulKHAOS@proton.me>
2026-01-29 04:31:53 -0300

    Update bot.js

--- client/bot.js
+++ client/bot.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const fs = require("fs");
-const config = require("../config.json");
 
 const client = new Client({
   intents: [
